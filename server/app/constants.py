c5e4f3b48452d8a336c99ad98f969100305b8dbe
HAITAO2003 <122141767+HAITAO2003@users.noreply.github.com>
2026-04-02 01:47:19 +0800

    more hyperparam tuning

--- server/app/constants.py
+++ server/app/constants.py
@@ -9,9 +9,9 @@ policies, and advanced optimizations such as caching and speculative decoding.
 MODEL_NAME = "Qwen/Qwen3-1.7B-FP8"
 MAX_MODEL_LENGTH = 512         
 KV_CACHE_DTYPE = "fp8"
-ENABLE_CHUNKED_PREFILL = False
+ENABLE_CHUNKED_PREFILL = True
 GPU_MEMORY_UTILIZATION = 0.85
-MAX_NUM_SEQS = 512
+MAX_NUM_SEQS = 320
 
 ENABLE_PREFIX_CACHING = True
 MAX_NUM_BATCHED_TOKENS = 16384
